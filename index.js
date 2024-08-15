f4918cb54481bfd0a984e9fbb9728b94272e5ac8
hasibur14 <hasib141415@gmail.com>
2024-08-15 20:23:33 +0600

    added: database connection in mongodb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 
@@ -9,7 +10,39 @@ app.use(cors());
 app.use(express.json());
 
 
- //here is connection on database......
+//here is connection on database......
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lfxjcnl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        const productCollection = client.db('primePickDB').collection('products');
+
+
+        //  get all products data
+        app.get('/products', async (req, res) => {
+            const result = await productCollection.find().toArray()
+            res.send(result)
+        });
+
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 
